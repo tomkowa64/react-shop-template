84216971b1258cacdd003bca2db81be5a01e5c7c
Tomasz Kowalski <tomkowa64@gmail.com>
2022-07-18 15:42:16 +0200

    Basic project structure setup

--- shop-template/src/index.tsx
+++ shop-template/src/index.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import Home from "./views/Home";
+import reportWebVitals from "./reportWebVitals";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./styles/index.sass";
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ shop-template/src/views/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import Nav from "../components/Nav";
+import "../styles/Home.sass";
+
+function Home() {
+  return (
+    <div className="App">
+      <Nav />
+      <h1>xD</h1>
+    </div>
+  );
+}
+
+export default Home;
